3c28ab02b2f3f18e27247153a7278e1274205a02
Ian Be <ian84be@gmail.com>
2019-03-19 16:35:31 -0500

    stretch: create react-app

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 const myRouter = require('./myRouter.js');
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 
 server.get('/', (req, res) => {
   res.send(`
